1b1dc7fc3ffe35ea6d32f21c144f4937eb3fea85
Antoine <antoine.valentin@gmail.com>
2026-06-09 11:04:29 +0200

    test: cover location permission mapper

---
+++ app/src/test/java/fr/arsenal/rallyetripmeter/ui/mapper/LocationPermissionMapperTest.kt
@@ -0,0 +1,29 @@
+package fr.arsenal.rallyetripmeter.ui.mapper
+
+import fr.arsenal.rallyetripmeter.android.permission.LocationPermissionState
+import fr.arsenal.rallyetripmeter.ui.model.UiLocationPermissionStatus
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class LocationPermissionMapperTest {
+    @Test
+    fun toUiLocationPermissionStatus_whenUnknown_returnsUnknown() {
+        val result = LocationPermissionState.Unknown.toUiLocationPermissionStatus()
+
+        assertEquals(UiLocationPermissionStatus.Unknown, result)
+    }
+
+    @Test
+    fun toUiLocationPermissionStatus_whenGranted_returnsGranted() {
+        val result = LocationPermissionState.Granted.toUiLocationPermissionStatus()
+
+        assertEquals(UiLocationPermissionStatus.Granted, result)
+    }
+
+    @Test
+    fun toUiLocationPermissionStatus_whenDenied_returnsDenied() {
+        val result = LocationPermissionState.Denied.toUiLocationPermissionStatus()
+
+        assertEquals(UiLocationPermissionStatus.Denied, result)
+    }
+}
